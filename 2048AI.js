7b4476021ca1858ab111b97890ee002b1f8faefb
Jamie Sinclair <jamiesinclair@gmail.com>
2014-04-17 10:54:38 -0400

    Random Generation and Swipe Methods

--- 2048AI.js
+++ 2048AI.js
@@ -12,7 +12,7 @@ var board = new function(size) {
 	
 	
 	
-	var blockSize = 80;
+	var blockSize = 60;
 	var blockMargin = 5;
 	b.generate = function() {
 		
@@ -47,30 +47,24 @@ var board = new function(size) {
 	};
 	
 	
-	b.countFreeSpaces = function() {
-		var count = 0;
+	b.freeSpaces = function() {
+		var spaces = [];
 		for(x=0;x<b.size;x++) {
 			for(y=0;y<b.size;y++) {
 				if(b.arr[x][y]==0) {
-					count++;
+					spaces.push({x:x,y:y});
 				}
 			}
 		}
 		
-		console.log(count);
-		return count;
+		return spaces;
 	};
 	
 	b.placeSquare = function(x,y,value) {
 		if(typeof x == 'undefined' || typeof y == 'undefined' || typeof value == 'undefined') {return false;}
 		
-		// internal...
-		var square = {value:value};
-		b.arr[x][y] = square;
-		
 		// visible
 		var squarePositionLeft = ((x*blockSize)+blockMargin+((x*2)*blockMargin))+'px';
-		
 		var squarePositionTop = ((y*blockSize)+blockMargin+((y*2)*blockMargin))+'px';
 		
 		var htmlSquare = $('<div>').addClass('square').addClass('val'+value)
@@ -79,19 +73,152 @@ var board = new function(size) {
 		
 		b.htmlBoard.append(htmlSquare);
 		
+		
+		
+		// internal...
+		var square = {value:value,html:htmlSquare};
+		b.arr[x][y] = square;
+		
+		
 	};
 	
 	
+	b.makeRandomSquare = function(values) {
+		values = (typeof values=='undefined' || !$.isArray(values)) ? [2, 2,4] : values;
+		
+		var finalVal = values[Math.floor(Math.random() * values.length)];
+		
+		var spaces = b.freeSpaces();
+		
+		var finalSpace = spaces[Math.floor(Math.random() * spaces.length)];
+		
+		b.placeSquare(finalSpace.x,finalSpace.y,finalVal);
+		
+	};
+	
+	b.moveSquare = function(x,y,dx,dy) {
+		
+		var newX = x+dx;
+		var newY = y+dy;
+		
+		var squarePositionLeft = ((newX*blockSize)+blockMargin+((newX*2)*blockMargin))+'px';
+		var squarePositionTop = ((newY*blockSize)+blockMargin+((newY*2)*blockMargin))+'px';
+		
+		
+		
+		// visible
+		var div = b.arr[x][y].html.animate({left:squarePositionLeft,top:squarePositionTop},1000);
+		
+		// internal
+		var temp = b.arr[x][y];
+		b.arr[x][y] = 0;
+		b.arr[newX][newY]=temp;
+	};
+	
+	b.swipe = function(direction) {
+		// console.log('swipe '+direction);
+		var x,y,dx,dy=0;
+		
+		// case, 'left'
+		if(direction=='left') {
+			for(x=1;x<b.size;x++) {
+				for(y=0;y<b.size;y++) {
+					if(b.arr[x][y]!=0) {
+						for(dx=0,count=0;count<x;count++,dx--) {
+							// console.log((x+(dx-1)));
+							if(b.arr[(x+(dx-1))][y]==0) {
+								continue;
+							}else {
+								break;
+							}
+						}
+						b.moveSquare(x,y,dx,0);
+						// console.log('move square at '+x+','+y+' by dx=='+dx);
+					}
+				}
+			}
+		} // eof 'left'
+		
+		
+		// case, 'right'
+		if(direction=='right') {
+			for(x=b.size-2;x>=0;x--) {
+				for(y=0;y<b.size;y++) {
+					if(b.arr[x][y]!=0) {
+						for(dx=0,count=0;count<(b.size-x-1);count++,dx++) {
+							// console.log((x+(dx+1)));
+							if(b.arr[(x+(dx+1))][y]==0) {
+								continue;
+							}else {
+								break;
+							}
+						}
+						b.moveSquare(x,y,dx,0);
+						// console.log('move square at '+x+','+y+' by dx=='+dx);
+					}
+				}
+			}
+		}
+		
+		
+		// case, 'up'
+		if(direction=='up') {
+			for(y=1;y<b.size;y++) {
+				for(x=0;x<b.size;x++) {
+					if(b.arr[x][y]!=0) {
+						// console.log(x+','+y);
+						for(dy=0,count=0;count<y;count++,dy--) {
+							// console.log((y+(dy+1)));
+							if(b.arr[x][(y+(dy-1))]==0) {
+								continue;
+							}else {
+								break;
+							}
+						}
+						b.moveSquare(x,y,0,dy);
+						// console.log('move square at '+x+','+y+' by dy=='+dy);
+					}
+				}
+			}
+		}
+		
+		
+		// case, 'down'
+		if(direction=='down') {
+			for(y=b.size-2;y>=0;y--) {
+				for(x=0;x<b.size;x++) {
+					if(b.arr[x][y]!=0) {
+						// console.log(x+','+y);
+						for(dy=0,count=0;count<(b.size-y-1);count++,dy++) {
+							// console.log((y+(dy+1)));
+							if(b.arr[x][(y+(dy+1))]==0) {
+								continue;
+							}else {
+								break;
+							}
+						}
+						b.moveSquare(x,y,0,dy);
+						// console.log('move square at '+x+','+y+' by dy=='+dy);
+					}
+				}
+			}
+		}
+		
+	};
+	$(window).bind('keyup','left',function(){b.swipe('left');});
+	$(window).bind('keyup','right',function(){b.swipe('right');});
+	$(window).bind('keyup','up',function(){b.swipe('up');});
+	$(window).bind('keyup','down',function(){b.swipe('down');});
 	
 	// generate the board and return...
 	b.generate();
 	
-	console.log(b);
-	b.placeSquare(1,1,2);
-	b.placeSquare(0,0,32);
-	b.placeSquare(0,3,4);
+	// console.log(b);
+	// b.placeSquare(1,1,2);
+	// b.placeSquare(0,0,32);
+	// b.placeSquare(0,3,4);
 	
-	b.countFreeSpaces();
+	// console.log(b.freeSpaces());
 	return b;
 };
 
--- README.md
+++ README.md
@@ -3,12 +3,11 @@
 I'm building the 2048 game so I can experiment with different approaches to solving it algorithmically.
 
 
-To do
+To do:
 
 
-
-- randomly place a square in an open space
-- swipe to move squares across the board
+- (4/17/14) randomly place a square in an open space
+- (4/17/14) swipe squares across the board
 - merge function that joins two squares of the same value when bumped by a swipe
 
 
